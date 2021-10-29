d94af108171fe46f01cc78defb8b700e860b45e9
Leonardo Cardoso <leonardocardasilva16@gmail.com>
2021-10-28 22:48:10 -0300

    Quarto commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,91 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+// Router
 import { AppRoutingModule } from './app-routing.module';
+// Firebase
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+// Layout
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+// Components
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { VerificarEmailComponent } from './verificar-email/verificar-email.component';
+import { EsqueceuSenhaComponent } from './esqueceu-senha/esqueceu-senha.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MateriaPageComponent } from './materia-page/materia-page.component';
+import { InfoComponent } from './info/info.component';
+import { AjudaComponent } from './ajuda/ajuda.component';
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from  '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    LoginComponent,
+    CadastroComponent,
+    PerfilComponent,
+    VerificarEmailComponent,
+    EsqueceuSenhaComponent,
+    AdminDashboardComponent,
+    HomePageComponent,
+    InfoComponent,
+    AjudaComponent,
+    MateriaPageComponent,
+    
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule,
+    LayoutModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'eduqui-7444d',
+    appId: '1:1027130101406:web:a0ab1fabddf7c22486758f',
+    storageBucket: 'eduqui-7444d.appspot.com',
+    apiKey: 'AIzaSyDwz5K33ImIf4PcZM16BvSivafYTI-LaME',
+    authDomain: 'eduqui-7444d.firebaseapp.com',
+    messagingSenderId: '1027130101406',
+  },
   production: false
 };
 
